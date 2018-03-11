28528be225bf14122baeb5a1a0244d8d8ea921fc
Merrick Paq-Man <paqmanator@gmail.com>
2018-03-10 19:00:29 -0800

    Service worker and manifest working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDru4pntvPtZ4TCPOBs2pSXev3-vFpbJ8I',
+    authDomain: 'hackinguvic2018-1520710969657.firebaseapp.com',
+    databaseURL: 'https://hackinguvic2018-1520710969657.firebaseio.com',
+    projectId: 'hackinguvic2018-1520710969657',
+    storageBucket: 'hackinguvic2018-1520710969657.appspot.com',
+    messagingSenderId: '1066945515067'
+  }
 };
